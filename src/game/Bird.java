f6b97f42abecd2004ae4c3cf5a63f34ec3e287a3
User <User@WS20.r1802.tciii.org.tw>
2023-07-07 16:33:20 +0800

    0707

--- src/game/Bird.java
+++ src/game/Bird.java
@@ -7,96 +7,72 @@ import java.awt.event.MouseListener;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 
-public class Bird{
-    // 小鸟的属性
-    Image image;
-    int x, y;
-    int width, height;
-    int size;
-    double g;
-    double t;
-    double v0;
-    double speed;
-    double s;
-    
-    
+public class Bird {
+	// 小鸟的属性
+	Image image;
+	int x, y;
+	int width, height;
+	int size;
+	double g;
+	double t;
+	double v0;
+	double speed;
+	double s;
 
+	public Bird() throws Exception {
+		// 初始化小鸟的属性
+		image = ImageIO.read(getClass().getResource("/bird.png"));
+		width = image.getWidth(null);
+		height = image.getHeight(null);
+		x = 350;
+		y = 280;
+		size = 40;
+		// 重力加速度
+		g = 0.08;
+		// 初始下降速度
+		v0 = 0;
+		t = 0.01;
+		speed = v0;
+		s = 0;
 
-    public Bird() throws Exception {
-        // 初始化小鸟的属性
-        image = ImageIO.read(getClass().getResource("/bird.png"));
-        width = image.getWidth(null);
-        height = image.getHeight(null);
-        x = 350;
-        y = 280;
-        size = 40;
-        //重力加速度
-        g = 0.08;
-        //初始下降速度
-        v0 = 0;
-        t = 0.01;
-        speed = v0;
-        s = 0;
-        
-    }
+	}
 
-    // 飞行动作
-    public void fly() {
-    	speed = v0;
-   }
-    
+	// 飞行动作
+	public void fly() {
+		speed = v0;
+	}
 
-    // 移动一步
-    public void step() {
-    	speed += g;
-        y += speed;
-    }
+	// 移动一步
+	public void step() {
+		speed += g;
+		y += speed;
+	}
 
-    // 向上飞行
-    public void clickFly() {
-        speed = -1;
-        int jumpDistance = -20;
-        y += jumpDistance;
-    }
+	// 向上飞行
+	public void clickFly() {
+		speed = -1;
+		int jumpDistance = -20;
+		y += jumpDistance;
+	}
 
+	// 检测小鸟是否碰撞到地面
+	public boolean hit(Ground ground) {
+		boolean hit = y + size / 2 > ground.y;
+		if (hit) {
+			y = ground.y - size / 2;
+		}
+		return hit;
+	}
 
-    // 检测小鸟是否碰撞到地面
-    public boolean hit(Ground ground) {
-        boolean hit = y + size/2 > ground.y;
-        if (hit) {
-            y = ground.y - size/2;
-        }
-        return hit;
-    }
-
-    // 检测小鸟是否撞到柱子
-    public boolean hit(Column column) {
-        // 检测是否在柱子范围内
-        if (x > column.x - column.width/2 - size/2 && x < column.x + column.width/2 + size/2) {
-            if (y > column.y - column.gap/2 + size/2 && y < column.y + column.gap/2 - size/2) {
-                return false;  // 在柱子范围内且没有发生碰撞
-            }
-            return true;  // 在柱子范围内且发生碰撞
-        }
-        return false;  // 不在柱子范围内
-    }
+	// 检测小鸟是否撞到柱子
+	public boolean hit(Column column) {
+		// 检测是否在柱子范围内
+		if (x > column.x - column.width / 2 - size / 2 && x < column.x + column.width / 2 + size / 2) {
+			if (y > column.y - column.gap / 2 + size / 2 && y < column.y + column.gap / 2 - size / 2) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
